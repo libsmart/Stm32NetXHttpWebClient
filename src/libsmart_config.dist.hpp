2b3660cb0be008377635a65a6398af8376f37b6e
Roland Rusch <roland@rusch.lu>
2024-08-24 20:03:59 +0200

    #1 Create basic example for nucleo-f429zi

---
+++ src/libsmart_config.dist.hpp
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef LIBSMART_STM32NETXHTTPWEBCLIENT_LIBSMART_CONFIG_DIST_HPP
+#define LIBSMART_STM32NETXHTTPWEBCLIENT_LIBSMART_CONFIG_DIST_HPP
+
+#define LIBSMART_STM32NETXHTTPWEBCLIENT
+
+
+
+
+
+#endif
